75b28e74e85273fe7bd7bec461a81c4cf0cc1b12
KhrapkovIKM723B <Ihor.Khrapkov@infiz.khpi.edu.ua>
2024-05-27 13:57:48 +0300

    Commit All

---
+++ Lab_19_Khrspkov.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <locale>
+using namespace std;
+
+template <typename T>
+void bubbleSort(T array[], int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+
+        for (int j = 0; j < size - i - 1; j++) 
+        {
+            if (array[j] < array[j + 1]) 
+            {
+                T temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
+
+template <typename T>
+void printArray(T array[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    setlocale(LC_ALL, ""); 
+    const int sizeInt = 5;
+    int intArray[sizeInt] = { 5, 2, 9, 1, 5 };
+
+    const int sizeFloat = 6;
+    float floatArray[sizeFloat] = { 4.3, 2.1, 5.6, 1.8, 3.7, 2.9 };
+
+    const int sizeChar = 5;
+    char charArray[sizeChar] = { 'e', 'a', 'd', 'c', 'b' };
+
+    cout << "Початковий масив цілих чисел: ";
+    printArray(intArray, sizeInt);
+    bubbleSort(intArray, sizeInt);
+    cout << "Відсортований масив цілих чисел: ";
+    printArray(intArray, sizeInt);
+
+    cout << "Початковий масив дійсних чисел: ";
+    printArray(floatArray, sizeFloat);
+    bubbleSort(floatArray, sizeFloat);
+    cout << "Відсортований масив дійсних чисел: ";
+    printArray(floatArray, sizeFloat);
+
+    cout << "Початковий масив символів: ";
+    printArray(charArray, sizeChar);
+    bubbleSort(charArray, sizeChar);
+    cout << "Відсортований масив символів: ";
+    printArray(charArray, sizeChar);
+
+    return 0;
+}
